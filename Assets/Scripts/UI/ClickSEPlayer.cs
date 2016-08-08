c832b293fd968625c4cf388bdffa9d14da3788fb
KzoNag <nagamine.keizo@gmail.com>
2016-08-08 11:45:19 +0900

    Play SE when click buttons.

---
+++ Assets/Scripts/UI/ClickSEPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class ClickSEPlayer : MonoBehaviour, IPointerClickHandler
+{
+    public string audioEventName = "Decide";
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        bool play = true;
+
+        var selectable = GetComponent<Selectable>();
+        if(selectable != null)
+        {
+            play = selectable.interactable;
+        }
+
+        if(play)
+        {
+            HoloToolkit.Unity.UAudioManager.Instance.PlayEvent(audioEventName);
+        }
+    }
+}
